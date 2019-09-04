21e7f6d19bf9d63af89863f20e42cc1851b36534
SaurabhGujjar <saurabhpanwar127@gmail.com>
2019-09-04 17:26:44 +0530

    put, delete, patch and post functions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,9 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
         'Uses HTTP method',
@@ -15,3 +19,25 @@ class HelloApiView(APIView):
         'Is mapped'
         ]
         return Response({'message':'Hello', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        return Response({'method':'put'})
+
+
+
+    def patch(self, request, pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
