6d210b0ab43f074dd4daa885e1550e44d5abb08b
SaurabhGujjar <saurabhpanwar127@gmail.com>
2019-09-05 17:12:20 +0530

    added CRUD

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -46,6 +46,7 @@ class HelloApiView(APIView):
 
 
 class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
     def list(self, request):
         a_viewset = [
         'Uses action',
@@ -53,3 +54,25 @@ class HelloViewSet(viewsets.ViewSet):
         'provides'
         ]
         return Response({'a_viewset':a_viewset, 'message':'Hello!'})
+
+    def create(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method':"DELETE"})
