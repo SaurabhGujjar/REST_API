adfd925d57c9c307f94cb6a9f8f9e3463d075e14
SaurabhGujjar <saurabhpanwar127@gmail.com>
2019-09-09 16:17:03 +0530

    profile feeds are added

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from profiles_api.models import UserProfile
+from profiles_api.models import UserProfile, ProfileFeedItem
 
 
 # admin.site.register(UserProfileManager)
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
